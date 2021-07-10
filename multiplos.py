2df0e354bc6aebbf4afb1e29173d72d7920b4670
Matheus Martins <matheusmmartins8@gmail.com>
2021-07-10 16:24:53 -0300

    Correção de possíveis erros de execução.

--- multiplos.py
+++ multiplos.py
@@ -5,6 +5,10 @@ Exercício numero 18 - Encontrando números múltiplos de um número x dentro de
 Data: 10/07/2021
 """
 
+# Apresentação:
+print('Programa para verificar quantos números múltiplos de um número n existem dentro de um intervalo. \n')
+
+# Declaração de variáveis e listas e entradas de dados.
 vetor = []
 multiplos = []
 numero = int(input('Entre com o valor de consulta: '))
@@ -12,12 +16,28 @@ inicio = int(input('Entre com o começo do intervalo: '))
 fim = int(input('Entre com o final do intervalo: '))
 passo = int(input('Entre com o passo do intervalo: '))
 
-for x in range(inicio, fim, passo):
-    vetor.append(x)
+# Verificando se os números são válidos:
 
-for x in vetor:
-    if x % numero == 0:
-        multiplos.append(x)
+if numero and passo != 0:
 
-print(f'Os números múltiplos de {numero} dentro do intervalo de {inicio} a {fim} ao passo {passo} são: {multiplos}.')
-print(f'Total de números múltiplos: {len(multiplos)}')
+    # Processamento de informações:
+    for x in range(inicio, fim+1, passo):
+        vetor.append(x)
+
+    for x in vetor:
+        if x % numero == 0:
+            multiplos.append(x)
+
+    # Saída de dados:
+    print(f'Os números múltiplos de {numero} dentro do intervalo de {inicio} a {fim} ao passo {passo} são: {multiplos}.')
+    print(f'Total de números múltiplos: {len(multiplos)} \n')
+    print('Obrigado por utilizar meu programa!')
+
+elif numero == 0:
+    print('O número de consulta não pode ser 0 (zero)')
+
+elif passo == 0:
+    print('O número de passo não pode ser 0 (zero)')
+
+else:
+    print('Algo deu errado no programa. Pedimos desculpas.')
